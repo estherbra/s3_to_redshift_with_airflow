88e1667a7ee438427ea292f608bc7f513459a488
Esther Brasileiro <brasileiroesther@gmail.com>
2022-02-23 18:36:40 -0300

    error fixes

--- passo 3/landing_table.sql
+++ passo 3/landing_table.sql
@@ -1,11 +1,3 @@
-CREATE TABLE IF NOT exists keycash_schema.LANDING_TABLE (
-	id VARCHAR PRIMARY KEY,
-	name VARCHAR ( 20 ) NULL,
-	idade integer NULL,
-	credito_solicitado integer NULL,
-	data_solicitacao VARCHAR NULL
-);
-
 ALTER TABLE keycash_schema.landing_table ADD COLUMN new_date timestamptz;
 UPDATE keycash_schema.landing_table SET new_date = to_timestamp(data_solicitacao, 'MM/DD/YYYY HH24:MI');
 ALTER TABLE keycash_schema.landing_table DROP COLUMN data_solicitacao;
